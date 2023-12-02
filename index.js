76e42d1e5c0e255371cd062cc175d75e4c89c829
Maruf12122 <149792231+Maruf12122@users.noreply.github.com>
2023-12-02 07:05:38 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+document.querySelector(".nl").addEventListener("click",function(){
+    // var x = document.getElementById("mm");
+    var x = document.querySelector(".navmain");
+    var op = document.querySelector(".close");
+    var y =document.querySelector(".nl")
+    x.classList.toggle("navon");
+    op.classList.toggle("open")
+    y.classList.toggle("xm")
+    // x.style.display="block"
+})
+document.querySelector(".close").addEventListener("click",function(){
+    // var x = document.getElementById("mm");
+    var x = document.querySelector(".navmain");
+    var op = document.querySelector(".close");
+    var y =document.querySelector(".nl")
+    x.classList.toggle("navon");
+    op.classList.toggle("open")
+    y.classList.toggle("xm")
+    // x.style.display="block"
+})
